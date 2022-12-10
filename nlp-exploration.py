3d722e86065a11a59b67fe7ffaac63793add4414
sarthakagrawal <seemanjeev@gmail.com>
2022-12-10 05:12:42 -0500

    adds venv and exploratory nlp analysis

--- nlp-exploration.py
+++ nlp-exploration.py
@@ -35,9 +35,11 @@ q_enhanced = q_enhanced[['question_id', 'title', 'title_short',
                          'description', 'resolution', 'resolution_comment',
                          'categories', 'mp_p', 'cp_p']].\
     drop_duplicates(subset=['title'])
+q_enhanced = q_enhanced[q_enhanced.resolution_comment == "resolved"]
+q_enhanced = q_enhanced[q_enhanced.mp_p.notnull()]
 
-print(q_enhanced.info())
-print(q_enhanced.shape)
+# print(q_enhanced.info())
+# print(q_enhanced.shape)
 # # print(q_only.tail())
 
 q_enhanced['description_len'] = q_enhanced['description'].str.len()
@@ -47,29 +49,38 @@ q_enhanced['mp_accuracy'] = q_enhanced['resolution'].subtract(
     q_enhanced['mp_p']).abs()
 # print(q_enhanced['accuracy'])
 
+print(q_enhanced['mp_accuracy'].isna().sum())
+
 # get a sense for distribution of data
 q_enhanced.plot(x="description_len", y=[
                 "cp_accuracy", "mp_accuracy"], kind="bar", figsize=(9, 8))
-mp.show()
+# mp.show()
 
 # plot linear regressions against our points!
 linear_regressor_cp = LinearRegression()  # create object for the class
 # perform linear regression
+# print(type(]))
+# print(type(q_enhanced['cp_accuracy']))
+
+var_len = q_enhanced['description_len'].values.reshape(-1, 1)
+var_cp_acc = q_enhanced['cp_accuracy'].values.reshape(-1, 1)
+var_mp_acc = q_enhanced['mp_accuracy'].values.reshape(-1, 1)
+
 linear_regressor_cp.fit(
-    q_enhanced['description_len'], q_enhanced['cp_accuracy'])
+    var_len, var_cp_acc)
 linear_regressor_mp = LinearRegression()
 linear_regressor_mp.fit(
-    q_enhanced['description_len'], q_enhanced['mp_accuracy'])
+    var_len, var_mp_acc)
 
 cp_pred = linear_regressor_cp.predict(
-    q_enhanced['cp_accuracy'])
+    var_cp_acc)
 
 mp_pred = linear_regressor_mp.predict(
-    q_enhanced['mp_accuracy'])  # make predictions
+    var_mp_acc)  # make predictions
 
-mp.scatter(q_enhanced['description_len'], [
-    q_enhanced['cp_accuracy'], q_enhanced['mp_accuracy']])
-mp.plot(q_enhanced['description_len'], [
-        q_enhanced['cp_accuracy'], q_enhanced['mp_accuracy']],
-        color=['red', 'blue'])
+mp.scatter(var_len, [
+    var_cp_acc, var_mp_acc])
+mp.plot(var_len, [
+    var_cp_acc, var_mp_acc],  # q_enhanced['mp_accuracy'
+    color=['red', 'blue'])
 mp.show()
