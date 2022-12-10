be059a49c750d9fdd359bcfc4f864b66bb70989b
javipus <javier.prietop@estudiante.uam.es>
2022-12-10 01:08:21 +0100

    minor refactoring and utility function added

--- aggregation.py
+++ aggregation.py
@@ -1,10 +1,20 @@
 import numpy as np
+import pandas as pd
 from utils import p2l, l2p
 
 ln2 = np.log(2)
 
 
-def neyman_agg(q, get_d=None, relative_t=False, half_life=1):
+def get_aggregate(bdf, agg_func, agg_func_kwds={}):
+    qids = bdf['question_id'].unique()
+    qs = []
+    for qid in qids:
+        qs.append(agg_func(
+            bdf[bdf.question_id == qid], **agg_func_kwds))
+    return pd.concat(qs, ignore_index=True)
+
+
+def neyman_agg(q, get_d=None, relative_t=True, half_life=ln2, col_name='np'):
     q = q.sort_values(by=['t']).reset_index(drop=True)
     q['lo'] = q['prediction'].apply(p2l)
     if relative_t:
@@ -12,7 +22,7 @@ def neyman_agg(q, get_d=None, relative_t=False, half_life=1):
     else:
         dts = q.t.diff().replace(np.nan, 0)
 
-    get_d = get_d or lambda n: (n*(3*n**2-3*n+1)**(.5)-2) / (n**2-n-1)
+    get_d = get_d or neyman_opt
     ds = [get_d(n) for n in range(1, q.shape[0]+1)]
 
     los = np.array([])
@@ -26,6 +36,10 @@ def neyman_agg(q, get_d=None, relative_t=False, half_life=1):
         los = np.append(los,
                         ds[k] * np.inner(q.iloc[:k+1, :]['lo'], ws) / sum(ws))
 
-    q['np'] = l2p(los)
+    q[col_name] = l2p(los)
 
     return q
+
+
+def neyman_opt(n):
+    return n*((3*n**2-3*n+1)**(.5)-2) / (n**2-n-1)
