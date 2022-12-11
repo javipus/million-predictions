38dee9e7b4d8336a1a282a31585f38cacdd2f7f3
sarthakagrawal <seemanjeev@gmail.com>
2022-12-11 06:27:43 -0500

    refactors nlp code to be reusable and more readable

--- nlp-exploration.py
+++ nlp-exploration.py
@@ -1,9 +1,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as mp
-import scipy
 from utils import load_data
-
+from nlp import enhance_df, calc_corr_length_pred, standard_scatter_and_regression
 # make a dataframe for question data only
 q_only_data = load_data(continuous=False, question_only=True)
 q_only = pd.DataFrame(q_only_data["questions"]["binary"])
@@ -12,79 +11,28 @@ q_only = pd.DataFrame(q_only_data["questions"]["binary"])
 p_data = load_data(continuous=False, question_only=False)
 p_only = pd.DataFrame(p_data["predictions"]["binary"]).add_suffix("_p")
 
-# produce an enhanced question dataframe that includes key factors of analysis
-q_enhanced = q_only.merge(
-    p_only, how="left", left_on="question_id", right_on="question_id_p")
-q_enhanced = q_enhanced[['question_id', 'title', 'title_short',
-                         'description', 'resolution', 'resolution_comment',
-                         'categories', 'mp_p', 'cp_p']].\
-    drop_duplicates(subset=['title'])
-q_enhanced = q_enhanced[q_enhanced.resolution_comment == "resolved"]
-q_enhanced = q_enhanced[q_enhanced.mp_p.notnull()]
+# produce an enhanced question dataframe that includes key factors of analysis (mp, cp)
+q_enhanced = enhance_df(q_only, p_only)
 
-# Create new columns with factors we care about; text length and accuracy
-q_enhanced['description_len'] = q_enhanced['description'].str.len()
-# the below line normalizes the length for more intuitive regressions
-q_enhanced['description_len'] = np.divide(
-    q_enhanced['description_len'], q_enhanced['description_len'].max())
-q_enhanced['cp_accuracy'] = np.exp(q_enhanced['resolution'].subtract(
-    q_enhanced['cp_p'], 2))
-q_enhanced['mp_accuracy'] = np.exp(q_enhanced['resolution'].subtract(
-    q_enhanced['mp_p']))
+# produce overall regression
+# key variables for overall regression
+var_len = q_enhanced['description_len'].values.reshape(-1, 1)
+var_cp_acc = q_enhanced['cp_accuracy'].values.reshape(-1, 1)
+var_mp_acc = q_enhanced['mp_accuracy'].values.reshape(-1, 1)
 
+# overall regressions
+standard_scatter_and_regression(
+    x_axis=var_len, x_distribution=q_enhanced['description_len'], y_axis=var_cp_acc, chart_title="len_vs_cp")
+standard_scatter_and_regression(
+    x_axis=var_len, x_distribution=q_enhanced['description_len'], y_axis=var_mp_acc, chart_title="len_vs_mp")
 
+# get more detail for subcategory analysis
 q_flatten_categories = pd.get_dummies(
     q_enhanced['categories'].apply(pd.Series).stack()).sum(level=0)
 q_enhanced_categories = pd.concat([q_enhanced, q_flatten_categories], axis=1)
 
-
-def calc_corr_length_pred(df):
-    cat_correlations = pd.DataFrame(columns=["category", "var_len", "c_i_cp", "m_cp", "b_cp",
-                                             "var_cp_acc", "c_i_mp", "m_mp", "b_mp", "var_mp_acc", "t_test_cp", "p_value_cp", "t_test_mp", "p_value_mp"])
-    for cat in list(df.iloc[:, 12:]):
-        if df[cat].sum() < 10:
-            continue
-        cat_df = df[df[cat] > 0]
-        var_len = cat_df['description_len'].values.reshape(-1, 1)
-        var_cp_acc = cat_df['cp_accuracy'].values.reshape(-1, 1)
-        var_mp_acc = cat_df['mp_accuracy'].values.reshape(-1, 1)
-        if df[cat].sum() > 29:
-            # do an analysis based on normal distribution
-            m_1, b_1 = np.polyfit(cat_df['description_len'], var_cp_acc, 1)
-            m_2, b_2 = np.polyfit(cat_df['description_len'], var_mp_acc, 1)
-            c_i_1 = scipy.stats.norm.interval(
-                confidence=0.95, loc=np.mean(m_1), scale=scipy.stats.sem(cat_df['description_len']))
-            c_i_2 = scipy.stats.norm.interval(
-                confidence=0.95, loc=np.mean(m_2), scale=scipy.stats.sem(cat_df['description_len']))
-            summary_stats = {"category": cat, "var_len": var_len, "c_i_cp": c_i_1, "m_cp": m_1, "b_cp": b_1,
-                             "var_cp_acc": var_cp_acc, "c_i_mp": c_i_2, "m_mp": m_2, "b_mp": b_2, "var_mp_acc": var_mp_acc}
-
-        elif df[cat].sum() > 9:
-            # do a t test
-            tstat_cp, pvalue_cp = scipy.stats.ttest_ind(
-                cat_df['description_len'], var_cp_acc)
-            tstat_mp, pvalue_cp = scipy.stats.ttest_ind(
-                cat_df['description_len'], var_mp_acc)
-            summary_stats = {"category": cat, "var_len": var_len,
-                             "var_cp_acc": var_cp_acc, "var_mp_acc": var_mp_acc, "t_test_cp": tstat_cp, "p_value_cp": pvalue_cp, "t_test_mp": tstat_mp, "p_value_mp": pvalue_cp}
-        summary_stats_as_series = pd.Series(summary_stats)
-        cat_correlations = cat_correlations.append(
-            summary_stats_as_series, ignore_index=True)
-    return cat_correlations
-
-
-category_results = calc_corr_length_pred(q_enhanced_categories)
-
-# fig1 = mp.figure()
-# mp.scatter(var_len, var_cp_acc)
-# mp.plot(var_len, m_1*var_len+b_1)
-# mp.text(.832, 2.149, str(m_1))
-# mp.show()
-# fig1.savefig("figures/len_vs_cp.png")
-
-# fig2 = mp.figure()
-# mp.scatter(var_len, var_mp_acc)
-# mp.plot(var_len, m_2*var_len+b_2)
-# mp.text(.832, 2.149, str(m_2))
-# mp.show()
-# fig2.savefig("figures/len_vs_mp.png")
+# produce dataframe with breakdown of statistical significance and correlation for all categories with >=10 questions.
+category_results = calc_corr_length_pred(q_enhanced_categories, return_columns=["category", "var_len", "c_i_cp", "m_cp", "b_cp",
+                                                                                "var_cp_acc", "c_i_mp", "m_mp", "b_mp", "var_mp_acc", "t_test_cp",
+                                                                                "p_value_cp", "t_test_mp", "p_value_mp"])
+print(category_results)
--- nlp.py
+++ nlp.py
@@ -1,45 +1,70 @@
 import numpy as np
 import pandas as pd
-from utils import p2l, l2p
+import matplotlib.pyplot as mp
 
-ln2 = np.log(2)
-
-
-def get_aggregate(bdf, agg_func, agg_func_kwds={}):
-    qids = bdf['question_id'].unique()
-    qs = []
-    for qid in qids:
-        qs.append(agg_func(
-            bdf[bdf.question_id == qid], **agg_func_kwds))
-    return pd.concat(qs, ignore_index=True)
+import scipy
 
 
-def neyman_agg(q, get_d=None, relative_t=True, half_life=ln2, col_name='np'):
-    q = q.sort_values(by=['t']).reset_index(drop=True)
-    q['lo'] = q['prediction'].apply(p2l)
-    if relative_t:
-        dts = q.relative_t.diff().replace(np.nan, 0)
-    else:
-        dts = q.t.diff().replace(np.nan, 0)
-
-    get_d = get_d or neyman_opt
-    ds = [get_d(n) for n in range(1, q.shape[0]+1)]
-
-    los = np.array([])
-    ws = np.array([])
-
-    # NB this assumes row index ranges (0, q.shape[0]-1)
-    for k, row in q.iterrows():
-        if k > 0:
-            ws *= np.exp(-dts[k] * ln2 / half_life)
-        ws = np.append(ws, np.exp(row['reputation_at_t']))
-        los = np.append(los,
-                        ds[k] * np.inner(q.iloc[:k+1, :]['lo'], ws) / sum(ws))
-
-    q[col_name] = l2p(los)
-
-    return q
+def enhance_df(main_df, new_df):
+    enhanced_df = main_df.merge(
+        new_df, how="left", left_on="question_id", right_on="question_id_p")
+    enhanced_df = enhanced_df[['question_id', 'title', 'title_short',
+                              'description', 'resolution', 'resolution_comment',
+                               'categories', 'mp_p', 'cp_p']].\
+        drop_duplicates(subset=['title'])
+    enhanced_df = enhanced_df[enhanced_df.resolution_comment == "resolved"]
+    enhanced_df = enhanced_df[enhanced_df.mp_p.notnull()]
+    # Create new columns with factors we care about; text length and accuracy
+    enhanced_df['description_len'] = enhanced_df['description'].str.len()
+    # the below line normalizes the length for more intuitive regressions
+    enhanced_df['description_len'] = np.divide(
+        enhanced_df['description_len'], enhanced_df['description_len'].max())
+    enhanced_df['cp_accuracy'] = np.exp(enhanced_df['resolution'].subtract(
+        enhanced_df['cp_p'], 2))
+    enhanced_df['mp_accuracy'] = np.exp(enhanced_df['resolution'].subtract(
+        enhanced_df['mp_p']))
+    return enhanced_df
 
 
-def neyman_opt(n):
-    return n*((3*n**2-3*n+1)**(.5)-2) / (n**2-n-1)
+def standard_scatter_and_regression(x_axis, x_distribution, y_axis, chart_title):
+    fig = mp.figure()
+    m_1, b_1 = np.polyfit(x_distribution, y_axis, 1)
+    mp.scatter(x_axis, y_axis)
+    mp.plot(x_axis, m_1*x_axis+b_1)
+    mp.text(.832, 2.149, str(m_1))
+    fig.savefig("figures/" + chart_title + ".png")
+    mp.close(fig)
+
+
+def calc_corr_length_pred(df, return_columns):
+    cat_correlations = pd.DataFrame(columns=return_columns)
+    for cat in list(df.iloc[:, 12:]):
+        if df[cat].sum() < 10:
+            continue
+        cat_df = df[df[cat] > 0]
+        var_len = cat_df['description_len'].values.reshape(-1, 1)
+        var_cp_acc = cat_df['cp_accuracy'].values.reshape(-1, 1)
+        var_mp_acc = cat_df['mp_accuracy'].values.reshape(-1, 1)
+        if df[cat].sum() > 29:
+            # do an analysis based on normal distribution
+            m_1, b_1 = np.polyfit(cat_df['description_len'], var_cp_acc, 1)
+            m_2, b_2 = np.polyfit(cat_df['description_len'], var_mp_acc, 1)
+            c_i_1 = scipy.stats.norm.interval(
+                confidence=0.95, loc=np.mean(m_1), scale=scipy.stats.sem(cat_df['description_len']))
+            c_i_2 = scipy.stats.norm.interval(
+                confidence=0.95, loc=np.mean(m_2), scale=scipy.stats.sem(cat_df['description_len']))
+            summary_stats = {"category": cat, "var_len": var_len, "c_i_cp": c_i_1, "m_cp": m_1, "b_cp": b_1,
+                             "var_cp_acc": var_cp_acc, "c_i_mp": c_i_2, "m_mp": m_2, "b_mp": b_2, "var_mp_acc": var_mp_acc}
+
+        elif df[cat].sum() > 9:
+            # do a t test
+            tstat_cp, pvalue_cp = scipy.stats.ttest_ind(
+                cat_df['description_len'], var_cp_acc)
+            tstat_mp, pvalue_cp = scipy.stats.ttest_ind(
+                cat_df['description_len'], var_mp_acc)
+            summary_stats = {"category": cat, "var_len": var_len,
+                             "var_cp_acc": var_cp_acc, "var_mp_acc": var_mp_acc, "t_test_cp": tstat_cp, "p_value_cp": pvalue_cp, "t_test_mp": tstat_mp, "p_value_mp": pvalue_cp}
+        summary_stats_as_series = pd.Series(summary_stats)
+        cat_correlations = cat_correlations.append(
+            summary_stats_as_series, ignore_index=True)
+    return cat_correlations
